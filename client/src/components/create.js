ec29923902374d14a7b6cdfc8ae258448dc8170a
Omaur B <omaurbliss@gmail.com>
2021-10-31 14:17:37 -0500

    changed components

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -39,20 +39,20 @@ export default class Create extends Component {
   }
  
 // This function will handle the submission.
-  onSubmit(e) {
+onSubmit(e) {
     e.preventDefault();
- 
+
     // When post request is sent to the create url, axios will add a new record(newperson) to the database.
     const newperson = {
       person_name: this.state.person_name,
       person_position: this.state.person_position,
       person_level: this.state.person_level,
     };
- 
+
     axios
       .post("http://localhost:5000/record/add", newperson)
       .then((res) => console.log(res.data));
- 
+
     // We will empty the state after posting the data to the database
     this.setState({
       person_name: "",
